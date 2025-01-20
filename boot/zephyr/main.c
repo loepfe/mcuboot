e99481c3fc5eacc51c04a6c10bdf894486bb3a99
Marco Schuler <marco.schuler@users.noreply.github.com>
2025-01-20 12:49:10 +0100

    WIP: OTFAD off/on (off working)

--- boot/zephyr/main.c
+++ boot/zephyr/main.c
@@ -16,6 +16,10 @@
  * limitations under the License.
  */
 
+#include <fsl_otfad.h>
+#include <fsl_flexspi.h>
+#include <MIMXRT1176_cm7.h>
+
 #include <assert.h>
 #include <zephyr/kernel.h>
 #include <zephyr/devicetree.h>
@@ -127,7 +131,7 @@ K_SEM_DEFINE(boot_log_sem, 1, 1);
 #define ZEPHYR_BOOT_LOG_STOP() do { } while (false)
 #endif /* defined(CONFIG_LOG) && !defined(ZEPHYR_LOG_MODE_IMMEDIATE) && \
         * !defined(ZEPHYR_LOG_MODE_MINIMAL)
-	*/
+        */
 
 BOOT_LOG_MODULE_REGISTER(mcuboot);
 
@@ -243,7 +247,7 @@ static void do_boot(struct boot_rsp *rsp)
 
 #ifndef CONFIG_SOC_FAMILY_ESPRESSIF_ESP32
 
-#define SRAM_BASE_ADDRESS	0xBE030000
+#define SRAM_BASE_ADDRESS       0xBE030000
 
 static void copy_img_to_SRAM(int slot, unsigned int hdr_offset)
 {
@@ -409,6 +413,149 @@ static void boot_serial_enter()
 }
 #endif
 
+// __attribute__((section(".itcm"))) void do_otfad_stuff(void)
+// {
+//     const uintptr_t address = 0x60104000;
+//     const uint32_t* pointer = (uint32_t*)address;
+//     const uint32_t value0 = *pointer;
+//     BOOT_LOG_INF("OTFAD2 is ON.  Value at address %p is %08X", pointer,  value0);
+
+//     for (int i = 0; i < 4; ++i) {
+//       BOOT_LOG_INF("OTFAD2->CTX[%d].RGD_W0 = 0x%08X", i, OTFAD2->CTX[i].RGD_W0);
+//       BOOT_LOG_INF("OTFAD2->CTX[%d].RGD_W1 = 0x%08X", i, OTFAD2->CTX[i].RGD_W1);
+//     }
+//     BOOT_LOG_INF("----------------------------------------------------------------------");
+
+//     /*--- BEGIN CRITICAL SECTION ---------------------------------------------*/
+//     unsigned int lock = irq_lock();
+
+//     // Switch off OTFAD
+//     const uint32_t otfad_ctx0_rgd_w1 = OTFAD2->CTX[0].RGD_W1;
+//     OTFAD2->CTX[0].RGD_W1 = OTFAD2->CTX[0].RGD_W0;
+
+//     for (int i = 0; i < 4; ++i) {
+//       BOOT_LOG_INF("OTFAD2->CTX[%d].RGD_W0 = 0x%08X", i, OTFAD2->CTX[i].RGD_W0);
+//       BOOT_LOG_INF("OTFAD2->CTX[%d].RGD_W1 = 0x%08X", i, OTFAD2->CTX[i].RGD_W1);
+//     }
+
+//    /* Do software reset or clear AHB buffer directly. */
+//    FLEXSPI2->AHBCR |= FLEXSPI_AHBCR_CLRAHBRXBUF_MASK;
+//    FLEXSPI2->AHBCR &= ~FLEXSPI_AHBCR_CLRAHBRXBUF_MASK;
+
+//     /* Invalidate data cache. At this moment, the flash memory is considered
+//        data since no code is not executed from it.
+//     OTFAD2 region start address: 0x60100400
+//     OTFAD2 region end address  : 0x6013F000 */
+//     SCB_InvalidateDCache_by_Addr ((void*)(0x60100400u), 0x6013F000u - 0x60100400u);
+//     /* END CRITICAL SECTION */
+//     irq_unlock(lock);
+//     /*--- END CRITICAL SECTION -----------------------------------------------*/
+
+//     const uint32_t value1 = *pointer;
+//     BOOT_LOG_INF("OTFAD2 is OFF. Value at address %p is %08X", pointer,  value1);
+
+//     /*--- BEGIN CRITICAL SECTION ---------------------------------------------*/
+//     lock = irq_lock();
+//     // Switch on OTFAD
+//     OTFAD2->CTX[0].RGD_W1 = otfad_ctx0_rgd_w1;
+
+//     for (int i = 0; i < 4; ++i) {
+//       BOOT_LOG_INF("OTFAD2->CTX[%d].RGD_W0 = 0x%08X", i, OTFAD2->CTX[i].RGD_W0);
+//       BOOT_LOG_INF("OTFAD2->CTX[%d].RGD_W1 = 0x%08X", i, OTFAD2->CTX[i].RGD_W1);
+//     }
+//     BOOT_LOG_INF("----------------------------------------------------------------------");
+
+//    /* Do software reset or clear AHB buffer directly. */
+//    FLEXSPI2->AHBCR |= FLEXSPI_AHBCR_CLRAHBRXBUF_MASK;
+//    FLEXSPI2->AHBCR &= ~FLEXSPI_AHBCR_CLRAHBRXBUF_MASK;
+
+//     /* Invalidate data cache. At this moment, the flash memory is considered
+//        data since no code is not executed from it.
+//     OTFAD2 region start address: 0x60100400
+//     OTFAD2 region end address  : 0x6013F000 */
+//     SCB_InvalidateDCache_by_Addr ((void*)(0x60100400u), 0x6013F000u - 0x60100400u);
+//     /* END CRITICAL SECTION */
+//     irq_unlock(lock);
+//     /*--- END CRITICAL SECTION -----------------------------------------------*/
+
+//     const uint32_t value2 = *pointer;
+//     BOOT_LOG_INF("OTFAD2 is ON.  Value at address %p is %08X", pointer,  value2);
+// }
+
+__attribute__((section(".itcm"))) void do_otfad_stuff(void)
+{
+    const uintptr_t address = 0x60104000;
+    const uint32_t* pointer = (uint32_t*)address;
+    const uint32_t value0 = *pointer;
+    BOOT_LOG_INF("OTFAD2 is ON.  Value at address %p is %08X", pointer,  value0);
+
+    /*--- BEGIN CRITICAL SECTION ---------------------------------------------*/
+    unsigned int lock = irq_lock();
+    // Switch off OTFAD
+    uint32_t iomuxc_gpr35 = IOMUXC_GPR->GPR35;
+    BOOT_LOG_INF("IOMUXC_GPR35 = 0x%08X", iomuxc_gpr35);
+    iomuxc_gpr35 ^= IOMUXC_GPR_GPR35_FLEXSPI2_OTFAD_EN_MASK;
+    BOOT_LOG_INF("Writing 0x%08X to GPR35", iomuxc_gpr35);
+    IOMUXC_GPR->GPR35 = iomuxc_gpr35;
+    BOOT_LOG_INF("IOMUXC_GPR35 = 0x%08X", IOMUXC_GPR->GPR35);
+
+    uint32_t otfad2_cr = OTFAD2->CR;
+    BOOT_LOG_INF("OTFAD2_CR    = 0x%08X", otfad2_cr);
+    otfad2_cr ^= OTFAD_CR_GE_MASK | OTFAD_CR_SKBP_MASK | OTFAD_CR_KBPE_MASK;
+    BOOT_LOG_INF("Writing 0x%08X to CR", otfad2_cr);
+    OTFAD2->CR = otfad2_cr;
+    BOOT_LOG_INF("OTFAD2_CR    = 0x%08X", OTFAD2->CR);
+
+   /* Do software reset or clear AHB buffer directly. */
+   FLEXSPI2->AHBCR |= FLEXSPI_AHBCR_CLRAHBRXBUF_MASK;
+   FLEXSPI2->AHBCR &= ~FLEXSPI_AHBCR_CLRAHBRXBUF_MASK;
+
+    /* Invalidate data cache. At this moment, the flash memory is considered
+       data since no code is not executed from it.
+    OTFAD2 region start address: 0x60100400
+    OTFAD2 region end address  : 0x6013F000 */
+    SCB_InvalidateDCache_by_Addr ((void*)(0x60100400u), 0x6013F000u - 0x60100400u);
+    /* END CRITICAL SECTION */
+    irq_unlock(lock);
+    /*--- END CRITICAL SECTION -----------------------------------------------*/
+
+    const uint32_t value1 = *pointer;
+    BOOT_LOG_INF("OTFAD2 is OFF. Value at address %p is %08X", pointer,  value1);
+
+    /*--- BEGIN CRITICAL SECTION ---------------------------------------------*/
+    lock = irq_lock();
+    // Switch on OTFAD
+    otfad2_cr = OTFAD2->CR;
+    BOOT_LOG_INF("OTFAD2_CR    = 0x%08X", otfad2_cr);
+    otfad2_cr |= OTFAD_CR_GE_MASK | OTFAD_CR_SKBP_MASK | OTFAD_CR_KBPE_MASK;
+    BOOT_LOG_INF("Writing 0x%08X to CR", otfad2_cr);
+    OTFAD2->CR = otfad2_cr;
+    BOOT_LOG_INF("OTFAD2_CR    = 0x%08X", OTFAD2->CR);
+
+    // iomuxc_gpr35 = IOMUXC_GPR->GPR35;
+    // BOOT_LOG_INF("IOMUXC_GPR35 = 0x%08X", iomuxc_gpr35);
+    // iomuxc_gpr35 |= IOMUXC_GPR_GPR35_FLEXSPI2_OTFAD_EN_MASK;
+    // BOOT_LOG_INF("Writing 0x%08X to GPR35", iomuxc_gpr35);
+    // IOMUXC_GPR->GPR35 = iomuxc_gpr35;
+    // BOOT_LOG_INF("IOMUXC_GPR35 = 0x%08X", IOMUXC_GPR->GPR35);
+
+   /* Do software reset or clear AHB buffer directly. */
+   FLEXSPI2->AHBCR |= FLEXSPI_AHBCR_CLRAHBRXBUF_MASK;
+   FLEXSPI2->AHBCR &= ~FLEXSPI_AHBCR_CLRAHBRXBUF_MASK;
+
+    /* Invalidate data cache. At this moment, the flash memory is considered
+       data since no code is not executed from it.
+    OTFAD2 region start address: 0x60100400
+    OTFAD2 region end address  : 0x6013F000 */
+    SCB_InvalidateDCache_by_Addr ((void*)(0x60100400u), 0x6013F000u - 0x60100400u);
+    /* END CRITICAL SECTION */
+    irq_unlock(lock);
+    /*--- END CRITICAL SECTION -----------------------------------------------*/
+
+    const uint32_t value2 = *pointer;
+    BOOT_LOG_INF("OTFAD2 is ON.  Value at address %p is %08X", pointer,  value2);
+}
+
 int main(void)
 {
     struct boot_rsp rsp;
@@ -417,6 +564,10 @@ int main(void)
 
     MCUBOOT_WATCHDOG_SETUP();
     MCUBOOT_WATCHDOG_FEED();
+
+    k_msleep(1000);
+
+    do_otfad_stuff();
 
 #if !defined(MCUBOOT_DIRECT_XIP)
     BOOT_LOG_INF("Starting bootloader");
